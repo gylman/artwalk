95639c1fa7ed6d1f4cbccc0558c2149dff8d8254
Keonwoo Kim <me@kanu.kim>
2023-07-12 21:41:49 +0900

    feat: implement the bottom navigation

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,6 +1,23 @@
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import PersonIcon from "@mui/icons-material/Person";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import DirectionsWalkIcon from "@mui/icons-material/DirectionsWalk";
 import { Frame } from "./Frame";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useMemo } from "react";
+
+function getTabIndexFromLocation({ pathname }) {
+  if (pathname.startsWith("/profiles")) return 0;
+  if (pathname.startsWith("/favorites")) return 2;
+  return 1;
+}
 
 export function Layout({ children, ...props }) {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const index = useMemo(() => getTabIndexFromLocation(location), [location]);
+
   return (
     <Frame
       wrapperStyle={{
@@ -9,6 +26,35 @@ export function Layout({ children, ...props }) {
       {...props}
     >
       {children}
+
+      <BottomNavigation
+        showLabels
+        style={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          width: "100%",
+          zIndex: 20,
+        }}
+        value={index}
+        onChange={(event, newValue) => {
+          if (newValue === index) return;
+          navigate(
+            [
+              "/profiles",
+              "/challenges",
+              "/favorites",
+            ][newValue],
+          );
+        }}
+      >
+        <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+        <BottomNavigationAction
+          label="Challenges"
+          icon={<DirectionsWalkIcon />}
+        />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+      </BottomNavigation>
     </Frame>
   );
 }
---
+++ client/src/pages/profiles/index.jsx
@@ -0,0 +1,14 @@
+import { Layout } from "../../components/Layout";
+
+export function Profile() {
+  return (
+    <Layout
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      Profile!
+    </Layout>
+  );
+}
