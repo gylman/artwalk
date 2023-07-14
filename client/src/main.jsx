494411d0162b58b873f7e7a90184db6500cc31f6
gylman <gylmanbm@gmail.com>
2023-07-15 00:56:12 +0900

    Mint FOrm

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,81 +1,88 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material";
-import { grey, red } from "@mui/material/colors";
-import "./main.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { grey, red } from '@mui/material/colors';
+import './main.css';
 
-import { Home } from "./pages/home";
-import { Challenges } from "./pages/challenges";
-import { Walk } from "./pages/challenges-slug-walk";
-import { Finish } from "./pages/challenges-slug-finish";
-import { Similarity } from "./pages/challenges-slug-similarity";
-import { Profile } from "./pages/profiles";
-import { Favorites } from "./pages/favorites";
-import { MapProvider } from "./components/Map";
-import { Challenge } from "./components/Challenge";
+import { Home } from './pages/home';
+import { Challenges } from './pages/challenges';
+import { Walk } from './pages/challenges-slug-walk';
+import { Finish } from './pages/challenges-slug-finish';
+import { Similarity } from './pages/challenges-slug-similarity';
+import { Profile } from './pages/profiles';
+import { Favorites } from './pages/favorites';
+import { MapProvider } from './components/Map';
+import { Challenge } from './components/Challenge';
+import MintForm from './components/MintForm';
+import back from './assets/images/back.svg';
+import { challenges } from './constants';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Home />,
   },
   {
-    path: "/challenges",
+    path: '/challenges',
     element: <Challenges />,
   },
   {
-    path: "/challenges/:slug/:page",
+    path: '/challenges/:slug/:page',
     element: <Walk />,
   },
   {
-    path: "/challenges/:slug/finish",
+    path: '/challenges/:slug/finish',
     element: <Finish />,
   },
   {
-    path: "/challenges/:slug/similarity",
+    path: '/challenges/:slug/similarity',
     element: <Similarity />,
   },
   {
-    path: "/profiles",
+    path: '/profiles',
     element: <Profile />,
   },
   {
-    path: "/profiles/:id",
+    path: '/profiles/:id',
     element: <Profile />,
   },
   {
-    path: "/favorites",
+    path: '/favorites',
     element: <Favorites />,
   },
-  { path: "/test", element: <Challenge /> },
+  { path: '/test', element: <Challenge /> },
+  {
+    path: '/mint',
+    element: <MintForm imgUrl={challenges[0].imgUrl} iconUrl={back} />,
+  },
 ]);
 
 const theme = createTheme({
   palette: {
     primary: {
-      50: "#7135C7",
-      100: "#7135C7",
-      200: "#7135C7",
-      300: "#7135C7",
-      400: "#7135C7",
-      500: "#7135C7",
-      600: "#7135C7",
-      700: "#7135C7",
-      800: "#7135C7",
-      900: "#7135C7",
+      50: '#7135C7',
+      100: '#7135C7',
+      200: '#7135C7',
+      300: '#7135C7',
+      400: '#7135C7',
+      500: '#7135C7',
+      600: '#7135C7',
+      700: '#7135C7',
+      800: '#7135C7',
+      900: '#7135C7',
     },
     secondary: grey,
     warning: red,
   },
 });
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <MapProvider>
         <RouterProvider router={router} />
       </MapProvider>
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
