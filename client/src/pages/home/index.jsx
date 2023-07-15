1fe03dcc8ed754227210e6832fabc870bb7bd375
gylman <gylmanbm@gmail.com>
2023-07-16 05:58:27 +0900

    add signup buttons, change from walletconneced to user state, however not styled properly

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,99 +1,99 @@
-import { Box, Typography } from "@mui/material";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { Frame } from "../../components/Frame";
-import { PrimaryButton } from "../../components/PrimaryButton";
-import background from "./background.svg";
+import { Box, Typography } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Frame } from '../../components/Frame';
+import { PrimaryButton } from '../../components/PrimaryButton';
+import background from './background.svg';
+import '../../components/flow/config';
+import * as fcl from '@onflow/fcl';
 
 export function Home() {
+  const [user, setUser] = useState({ loggedIn: null });
+
+  useEffect(() => fcl.currentUser.subscribe(setUser), []);
+
   // TODO: connect wallet
-  const [isWalletConnected, setIsWalletConnected] = useState(false);
 
   return (
     <Frame
       wrapperStyle={{
         background: `url(${background})`,
-        backgroundSize: "cover",
-        color: "white",
-        backgroundPosition: "center",
+        backgroundSize: 'cover',
+        color: 'white',
+        backgroundPosition: 'center',
       }}
       style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
       }}
     >
       <Typography
-        variant="h1"
-        component="h1"
+        variant='h1'
+        component='h1'
         fontWeight={600}
-        fontSize="64px"
-        lineHeight="70px"
-        paddingTop="64px"
-        textAlign="center"
-        color="#9afcc5"
-        fontFamily="Mona Sans"
+        fontSize='64px'
+        lineHeight='70px'
+        paddingTop='64px'
+        textAlign='center'
+        color='#9afcc5'
+        fontFamily='Mona Sans'
       >
         Own Your Workout
       </Typography>
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "start",
-          alignItems: "center",
-          paddingBottom: "48px",
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'start',
+          alignItems: 'center',
+          paddingBottom: '48px',
         }}
       >
         <Typography
-          variant="h2"
-          component="h2"
-          fontFamily="Mona Sans"
+          variant='h2'
+          component='h2'
+          fontFamily='Mona Sans'
           style={{
-            width: "100%",
+            width: '100%',
             fontSize: 48,
             lineHeight: 1,
             fontWeight: 600,
-            textAlign: "center",
-            color: "#7135C7",
-            marginBottom: "16px",
+            textAlign: 'center',
+            color: '#7135C7',
+            marginBottom: '16px',
           }}
         >
           Touch grass
         </Typography>
         <Typography
-          variant="h2"
-          component="h2"
-          fontFamily="Mona Sans"
+          variant='h2'
+          component='h2'
+          fontFamily='Mona Sans'
           style={{
-            width: "100%",
+            width: '100%',
             fontSize: 24,
-            lineHeight: "30px",
+            lineHeight: '30px',
             fontWeight: 400,
-            textAlign: "center",
-            color: "#2B2B2B",
-            marginBottom: "56px",
+            textAlign: 'center',
+            color: '#2B2B2B',
+            marginBottom: '56px',
           }}
         >
-          Create & Mint Art<br />through Fitness
+          Create & Mint Art
+          <br />
+          through Fitness
         </Typography>
-        {isWalletConnected
-          ? (
-            <Link to="/challenges">
-              <PrimaryButton>
-                Go to challenge
-              </PrimaryButton>
-            </Link>
-          )
-          : (
-            <PrimaryButton
-              onClick={() => {
-                setIsWalletConnected(true);
-              }}
-            >
-              Connect wallet
-            </PrimaryButton>
-          )}
+        {user.loggedIn ? (
+          <Link to='/challenges'>
+            <PrimaryButton>Go to challenge</PrimaryButton>
+          </Link>
+        ) : (
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            <PrimaryButton onClick={fcl.logIn}> Connect wallet</PrimaryButton>
+            <PrimaryButton onClick={fcl.signUp}> Sign up</PrimaryButton>
+          </Box>
+        )}
       </Box>
     </Frame>
   );
